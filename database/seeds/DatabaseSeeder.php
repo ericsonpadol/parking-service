c30f65276b55c1c10830cc3fe049ee0d4b3bf936
Ericson Padol <ericson.padol@gmail.com>
2018-12-17 12:43:00 +0800

    update on database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
-
 use App\Vehicle;
 use App\ParkingSpace;
 use App\Subscriber;
@@ -10,22 +9,23 @@ use App\User;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         // $this->call(UsersTableSeeder::class);
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Vehicle::truncate();
-        ParkingSpace::truncate();
         User::truncate();
+        ParkingSpace::truncate();
         Model::unguard();
 
         $this->call('VehicleSeed');
         $this->call('UserSeed');
         $this->call('ParkingSpace');
     }
+
 }
